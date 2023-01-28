e8d6c07847abc3bc128684167a42c156121cb2fa
Tiranoe <jungwoo9543@gmail.com>
2023-01-28 18:11:28 -0500

    animation letters working

--- react-portfolio/src/components/Home/index.js
+++ react-portfolio/src/components/Home/index.js
@@ -1,19 +1,32 @@
 import { Link } from 'react-router-dom';
-import LOGO from '../../assets/images/LOGO.png'
+import { useState } from 'react';
+import AnimatedLetters from '../AnimatedLetters';
 import './index.scss';
 
+
 const Home = () => {
+    const [letterClass, setLetterClass] = useState('text-animate')
+    const nameArray = [' ', 'J', 'o', 'h', 'n',' ', 'N', 'a', 'm']
+    const jobArray = ['s', 'o', 'f', 't', 'w', 'a', 'r', 'e', ' ', 'e', 'n', 'g', 'i', 'n', 'e', 'e', 'r', '!']
 
     return (
         <div className='container home-page'>
             <div className='text-zone'>
-                <h1>Hi, <br /> My name is John Nam </h1>
-                <h1 className='initals'>Also known as
-                <img src={LOGO} alt='JJN' />
-                </h1>
+                <h1>
+                <span className={letterClass}>Hi, </span>    
                 <br />
-                Software Engineer
-                <h2>Data Engineer / QA Analyst</h2>
+                My name is 
+                <AnimatedLetters letterClass={letterClass}
+                strArray={nameArray}
+                idx={15} />
+                <br />
+                I am a...
+                <br />
+                <AnimatedLetters letterClass={letterClass}
+                strArray={jobArray}
+                idx={23} />
+                </h1>
+                <h2>Data Engineer / QA Analyst / Youtuber</h2>
                 <Link to ='/contact' className='flat-button'>REACH ME HERE</Link>
             </div>
         </div>
