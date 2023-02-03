951a826bda80e3e79408d6d40d5f0d91d78300f6
Tiranoe <jungwoo9543@gmail.com>
2023-02-03 18:04:18 -0500

    fixed the css where form was overlapped with description

--- react-portfolio/src/components/Contact/index.js
+++ react-portfolio/src/components/Contact/index.js
@@ -14,44 +14,44 @@ const Contact = () => {
     
     return (
         <>
-        <div className='container contact-page'>
-            <div className='text-zone'>
-                <h1>
-                    <AnimatedLetters
-                        letterClass={letterClass}
-                        strArray={['C', 'o', 'n', 't', 'a', 'c', 't', ' ', 'm', 'e']}
-                        idx={15}
-                    />
-                </h1>
-                <p>
-                    I am looking towards any opportunities to improve my skill set 
-                    in programming languages of Python and JavaScript. Please reach out via email
-                    @Jungwoo9543@gmail.com, and I would love to discuss anything regarding to my experiences!
-                </p>
-                <div className='contact-form'>
-                    <form>
-                        <ul>
-                            <li className='half'>
-                                <input type='text' name='name' placeholder='Name' required/>
-                            </li>
-                            <li className='half'>
-                                <input type='email' name='email' placeholder='Email' required/>
-                            </li>
-                            <li> 
-                                <input type='text' name='subject' placeholder='Subject' required/>
-                            </li>
-                            <li>
-                                <textarea name='message' placeholder='Message' required/>
-                            </li>
-                            <li>
-                                <input name='submit' className='flat-button' value='SEND'/>
-                            </li>
-                        </ul>
-                    </form>
+            <div className='container contact-page'>
+                <div className='text-zone'>
+                    <h1>
+                        <AnimatedLetters
+                            letterClass={letterClass}
+                            strArray={['C', 'o', 'n', 't', 'a', 'c', 't', ' ', 'm', 'e']}
+                            idx={15}
+                        />
+                    </h1>
+                    <p>
+                        I am looking towards any opportunities to improve my skill set 
+                        in programming languages of Python and JavaScript. Please reach out via email
+                        @Jungwoo9543@gmail.com, and I would love to discuss anything regarding to my experiences!
+                    </p>
+                    <div className="contact-form">
+                        <form>
+                            <ul>
+                                <li className='half'>
+                                    <input type='text' name='name' placeholder='Name' required/>
+                                </li>
+                                <li className='half'>
+                                    <input type='email' name='email' placeholder='Email' required/>
+                                </li>
+                                <li> 
+                                    <input type='text' name='subject' placeholder='Subject' required/>
+                                </li>
+                                <li>
+                                    <textarea name='message' placeholder='Message' required/>
+                                </li>
+                                <li>
+                                    <input name='submit' className='flat-button' value='SEND'/>
+                                </li>
+                            </ul>
+                        </form>
+                    </div>
                 </div>
             </div>
-        </div>
-        <Loader type='ball-grid-beat' />
+            <Loader type='ball-grid-beat' />
         </>
     )
 }
