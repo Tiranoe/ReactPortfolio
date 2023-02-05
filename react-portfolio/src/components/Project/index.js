a7701da7349d9887d52ab20fea13248524c6a1cb
Tiranoe <jungwoo9543@gmail.com>
2023-02-04 20:47:49 -0500

    Projects label started

--- react-portfolio/src/components/Project/index.js
+++ react-portfolio/src/components/Project/index.js
@@ -1,5 +1,34 @@
 import './index.scss'
+import { React, useEffect, useState } from 'react'
+import Loader from 'react-loaders'
+import AnimatedLetters from '../AnimatedLetters'
 
-const Project = () => 'Hello'
+const Project = () => {
+
+    const [letterClass, setLetterClass] = useState('text-animate')
+
+    useEffect(()=> {
+        return setTimeout(() => {
+            setLetterClass('text-animate-hover')
+        }, 2000)
+    }, [])
+
+    return (
+        <>
+            <div className='container about-page'>
+                <div className='text-zone'>
+                    <h1>
+                        <AnimatedLetters
+                            letterClass={letterClass}
+                            strArray={"Projects".split("")}
+                            idx={15}
+                        />
+                    </h1>
+                </div>
+            </div>
+            <Loader type='ball-grid-beat' />
+        </>
+    )
+}
 
 export default Project
