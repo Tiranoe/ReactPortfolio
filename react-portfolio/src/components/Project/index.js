e5b9f8ed23c4ed88e51e3b8e4ad5f706765bd7ba
Tiranoe <jungwoo9543@gmail.com>
2023-02-08 19:59:04 -0500

    successfully added each project images

--- react-portfolio/src/components/Project/index.js
+++ react-portfolio/src/components/Project/index.js
@@ -2,10 +2,11 @@ import './index.scss'
 import { React, useEffect, useState } from 'react'
 import Loader from 'react-loaders'
 import AnimatedLetters from '../AnimatedLetters'
+import projectData from '../../data/projects.json'
 
 const Project = () => {
-
     const [letterClass, setLetterClass] = useState('text-animate')
+    console.log(projectData)
 
     useEffect(()=> {
         const timer = setTimeout(() => {
@@ -17,10 +18,29 @@ const Project = () => {
         }
     })
 
+    const renderProjects = (projects) => {
+        return (
+            <div className="images-container">
+                {
+                    projects.map((proj, idx) => {
+                        return (
+                            <div className="image-box" key={idx}>
+                                <img 
+                                src={proj.cover} 
+                                className="project-gif"
+                                alt="project cover" />
+                            </div>
+                        )
+                    })
+                }
+            </div>
+        )
+    }
+
     return (
     <>
         <div className='container project-page'>
-            <div className='text-zone'>
+            <div className='page-title'>
                 <h1>
                     <AnimatedLetters
                         letterClass={letterClass}
@@ -28,27 +48,8 @@ const Project = () => {
                         idx={15}
                     />
                 </h1>
+                <div>{renderProjects(projectData.projects)}</div>
             </div>
-
-                <div className='projects_list'>
-                    <div className='card'></div>
-                    <div className='card-image'>
-                        <div className='project1'>
-                            <figure class='image-is-square'>
-                            <img src= "https://github.com/Tiranoe/adventureTime/blob/main/assets/adventuretimegif.gif?raw=true" alt = "adventureTimes"/> </figure>
-                        </div>
-                        <div className='project2'>
-                            <figure class='image-is-square'>
-                            <img src= "https://github.com/Tiranoe/Instagram-Cloning/blob/main/assets/instagram%20clone%20gif.gif?raw=true" alt = "instagramClone"/> </figure>
-                        </div>
-                        <div className='project3'>
-                            <img src= "https://github.com/Tiranoe/TAMA/blob/main/assets/ezgif-5-9cb8e12fd0.gif?raw=true" alt = "tamagatchi"/>
-                        </div>
-                        <div className='project4'>
-                            <img src= "https://github.com/Tiranoe/Travel-Cities/blob/main/assets/travelizer%20gif.gif?raw=true" alt = "travelizer"/>
-                        </div>
-                    </div>
-                </div>
             </div>
         <Loader type='ball-grid-beat' />
     </>
